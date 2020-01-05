0242e025f362677d314954f50cc8a19cdb112c9f
ngsiolei <ngsiolei@gmail.com>
2020-01-05 22:24:21 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService, PeriodicElement } from "./data.service";
+import { MatTableDataSource } from "@angular/material/table";
+import { interval } from "rxjs";
+
+const ITEMS_PER_PAGE = 9;
+const SECONDS_PER_PAGE = 5;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'ex09';
+export class AppComponent implements OnInit {
+  title = "ex09";
+  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
+  dataSource = new MatTableDataSource<PeriodicElement>([]);
+  page = 1;
+  secondsLeft = SECONDS_PER_PAGE;
+  timer = interval(1000);
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.getData(true);
+    this.startTimer();
+  }
+
+  private getData(firstTime: boolean = false) {
+    if (!firstTime && 0 !== this.secondsLeft) {
+      return;
+    }
+    this.dataService
+      .get(this.page, ITEMS_PER_PAGE)
+      .subscribe((data: PeriodicElement[]) => {
+        this.dataSource.data = data;
+        const hasNextPage = data && data[data.length - 1];
+        if (hasNextPage) {
+          this.page++;
+        } else {
+          this.page = 1;
+        }
+      });
+  }
+
+  private startTimer() {
+    this.timer.subscribe(n => {
+      if (this.secondsLeft > 0) {
+        this.secondsLeft--;
+      } else {
+        this.secondsLeft = SECONDS_PER_PAGE;
+      }
+      this.getData();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NoopAnimationsModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
